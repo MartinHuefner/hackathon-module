4991ad3070d5cbe8c253e9e375a16f782a60b1d5
MartinHuefner <116950916+MartinHuefner@users.noreply.github.com>
2026-06-17 16:16:39 +0200

    Enhance module documentation in server.js
    Updated module description and added command usage details.

--- server.js
+++ server.js
@@ -24,7 +24,19 @@ app.get('/test/hackathon-module', (req, res) => {
   <body>
     <img src="https://app.tech-riders.de/p/2364/photo" alt="module photo" style="max-width:100%;height:auto;" />
     <p>I was able to create a module</p>
-    <p>The module should only show the picture from https://app.tech-riders.de/p/2364/photo and say "I was able to create a module".</p>
+    <p>The following command was used with model qwen3-coder-30b-a3b-instruct to create this module:</p>
+    <p>#new create a new workspace for a module that should be part of a larger service-based app. 
+    The conventions for the app are stored in: https://github.com/noLXXIII/VibeCodedAdministration/blob/main/docs/TEAM-CONVENTIONS.md
+
+    The module should only show the picture from https://app.tech-riders.de/p/2364/photo
+
+    And say "I was able to create a module"</p>
+
+    <p>Code changes until it run with the app: <br>
+
+    Added: 1003 lines
+    Deleted: 19 lines
+    Total changed: 1022 lines</p>
   </body>
 </html>`);
 });
